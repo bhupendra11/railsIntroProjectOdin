f562176c5133b58780b3c1c9af72233d95d5ca4b
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2015-05-26 14:47:14 +0530

    form based workflow feature completed

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,5 +1,13 @@
 Blogger::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+ root to: 'articles#index'
+resources :articles
+
+
+end
+
+
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -53,9 +61,3 @@ Blogger::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-resources :articles
-
-
-
-end
